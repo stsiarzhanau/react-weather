c29afe165bbdc26a037b27446e9b25ddcef207b9
Aliaksandr Stsiarzhanau <sterzhanoff@gmail.com>
2016-11-26 19:33:53 +0300

    Add foundation and jQuery

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,10 @@ import Weather from 'Weather';
 import About from 'About';
 import Examples from 'Examples';
 
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,24 @@
-// const webpack = require('webpack');
+const webpack = require('webpack');
 // const path = require('path');
 
 module.exports = {
-  entry: './src/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './src/app.jsx',
+  ],
   output: {
     path: __dirname,
-    filename: './dist/bundle.js'
+    filename: './dist/bundle.js',
   },
-  resolve: {
-    root: __dirname,
-    alias: {
-      Main: 'src/components/Main.jsx',
-      Nav: 'src/components/Nav.jsx',
-      Weather: 'src/components/Weather.jsx',
-      WeatherForm: 'src/components/WeatherForm.jsx',
-      WeatherMessage: 'src/components/WeatherMessage.jsx',
-      About: 'src/components/About.jsx',
-      Examples: 'src/components/Examples.jsx',
-      openWeatherMap: 'src/api/openWeatherMap.jsx'
-    },
-    extensions: ['', '.js', '.jsx']
-  },
-  // devServer: {
-  //   inline: true;
-  //   contentBase: './build',
-  //   port: 3000
-  // },
   module: {
-    preLoaders: [
-      {
-        test: /\.jsx?$/,
-        loader: 'eslint',
-        exclude: /(node_modules|bower_components)/
-      }
-    ],
     loaders: [
       // code for webpack v2.1.0-beta23 and above
       // {
       //   enforce: 'pre',
       //   test: /\.jsx?$/,
       //   exclude: /(node_modules|bower_components)/,
-      //   loader: 'eslint'
+      //   loader: 'eslint',
       // },
       {
         test: /\.jsx?$/,
@@ -49,19 +27,54 @@ module.exports = {
         query: {
           presets: [
             'es2015',
-            'react'
-          ]
-        }
+            'react',
+          ],
+        },
       },
       // {
       //   test: /\.(png|jpg)$/,
-      //   loader: 'url-loader?limit2000'
+      //   loader: 'url-loader?limit2000',
       // },
       // {
       //   test: /\.scss$/,
-      //   loader: 'style-loader!css-loader!sass-loader'
-      // }
-    ]
+      //   loader: 'style-loader!css-loader!sass-loader',
+      // },
+    ],
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint',
+        exclude: /(node_modules|bower_components)/,
+      },
+    ],
   },
-  devtool: 'source-map'
-}
+  resolve: {
+    alias: {
+      Main: 'src/components/Main.jsx',
+      Nav: 'src/components/Nav.jsx',
+      Weather: 'src/components/Weather.jsx',
+      WeatherForm: 'src/components/WeatherForm.jsx',
+      WeatherMessage: 'src/components/WeatherMessage.jsx',
+      About: 'src/components/About.jsx',
+      Examples: 'src/components/Examples.jsx',
+      openWeatherMap: 'src/api/openWeatherMap.jsx',
+    },
+    root: __dirname,
+    extensions: ['', '.js', '.jsx'],
+  },
+  externals: {
+    jquery: 'jQuery',
+  },
+  devtool: 'source-map',
+  // devServer: {
+  //   inline: true,
+  //   contentBase: './build',
+  //   port: 3000,
+  // },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
+};
